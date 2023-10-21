5f8e1dfec9d6ce21107a6e4dae7393bc3b836b15
gieribac <geibaneza@unal.edu.co>
2023-10-21 14:33:14 -0500

    cambios guardados

---
+++ script.js
@@ -0,0 +1,90 @@
+class own {
+    constructor(tel, nombre, video) {
+        this.nombre = nombre;
+        this.tel = tel;
+        this.video = video;
+    }
+}
+
+var owns = [];
+const own1 = new own('0998334041', 'PABLO', 'https://firebasestorage.googleapis.com/v0/b/adnreckitt.appspot.com/o/ARAQUE%20FELIX%20PABLO%20LENIN.mp4?alt=media&token=e98f7da9-0807-40b6-908e-63c25d7a4f51')
+owns.push(own1);
+const own2 = new own('51998343275', 'GINO', 'https://firebasestorage.googleapis.com/v0/b/adnreckitt.appspot.com/o/GINO%20VERGARA.mp4?alt=media&token=b3ce2fed-b6c6-4f6c-8678-fce4b0023bdd')
+owns.push(own2);
+const own3 = new own('0995881156', 'ALEX', 'https://firebasestorage.googleapis.com/v0/b/adnreckitt.appspot.com/o/ALEX%20CAJAS.mp4?alt=media&token=3cb5b4e8-b469-4be0-a34b-fc3b5dd9f09a')
+owns.push(own3);
+const own4 = new own('0983513005', 'DARYCRIST', 'https://firebasestorage.googleapis.com/v0/b/adnreckitt.appspot.com/o/MARTINEZ%20NUnEZ%20DARYCRIST.mp4?alt=media&token=a76028b4-aa45-4ad0-8ba2-d2dc7dc3028f')
+owns.push(own4);
+const own5 = new own('0987329121', 'SOFIA', 'https://firebasestorage.googleapis.com/v0/b/adnreckitt.appspot.com/o/SOFIA%20UBIDIA.mp4?alt=media&token=a6c5fc87-5238-460c-81a3-d40dd786ab22')
+owns.push(own5);
+const own6 = new own('0989429353', 'LIZZA', 'https://firebasestorage.googleapis.com/v0/b/adnreckitt.appspot.com/o/LIZZA%20DALLE.mp4?alt=media&token=4efb8795-5ea8-42cf-9ccf-cb2f9e395775')
+owns.push(own6);
+const own7 = new own('0996964396', 'JESSENIA', 'https://firebasestorage.googleapis.com/v0/b/adnreckitt.appspot.com/o/BUSTAMAMANTE%20ASTUDILLO%20JESSENIA.mp4?alt=media&token=e0566312-3e9a-4aa0-b512-fb77debeb5b4')
+owns.push(own7);
+const own8 = new own('0984518420', 'LUIS', 'https://firebasestorage.googleapis.com/v0/b/adnreckitt.appspot.com/o/CAIZA%20AMORES%20LUIS%20ROLANDO.mp4?alt=media&token=59b76332-451b-4c2f-80f0-0ec9b5c55d32')
+owns.push(own8);
+const own9 = new own('0983511920', 'JAIRO', 'https://firebasestorage.googleapis.com/v0/b/adnreckitt.appspot.com/o/CEVALLOS%20VACA%20JAIRO%20LUIS.mp4?alt=media&token=8c38b566-d2e1-4b5b-9ea5-8244a0bd6906')
+owns.push(own9);
+const own10 = new own('0997345923', 'JOEL', 'https://firebasestorage.googleapis.com/v0/b/adnreckitt.appspot.com/o/COLLAGUAZO%20CORREA%20JOEL.mp4?alt=media&token=a2dfcfc3-1b00-47bf-a552-eabdf6f6a68a')
+owns.push(own10);
+const own11 = new own('0995553827', 'GEOVANNA', 'https://firebasestorage.googleapis.com/v0/b/adnreckitt.appspot.com/o/GALARZA%20MORILLO%20GEOVANNA.mp4?alt=media&token=a5473c41-b666-4b1d-8c26-eefd5434aeef')
+owns.push(own11);
+const own12 = new own('0988134653', 'HUGO', 'https://firebasestorage.googleapis.com/v0/b/adnreckitt.appspot.com/o/HIDALGO%20ROBAYO%20HUGO.mp4?alt=media&token=62e391aa-f73a-470d-b90e-f8fceb476e43')
+owns.push(own12);
+const own13 = new own('0984513117', 'ANA', 'https://firebasestorage.googleapis.com/v0/b/adnreckitt.appspot.com/o/VALAREZO%20ESPINOZA%20ANA%20GUADALUPE.mp4?alt=media&token=26c6911e-db07-4aad-a473-6c3d776ea239')
+owns.push(own13);
+const own14 = new own('0991615089', 'ARELLYS', 'https://firebasestorage.googleapis.com/v0/b/adnreckitt.appspot.com/o/ARCOS%20VELASTEGUI%20ARELLYS%20BECHARA.mp4?alt=media&token=af418484-b806-4cd0-99d6-ffa8d637fe40')
+owns.push(own14);
+const own15 = new own('0983507914', 'JESUSA', 'https://firebasestorage.googleapis.com/v0/b/adnreckitt.appspot.com/o/BRAVO%20ANTON%20JESUS%20MOSERRATE.mp4?alt=media&token=a96fd82a-f308-4660-bc42-7f4946e0225e')
+owns.push(own15);
+const own16 = new own('0994048264', 'ELIANA', 'https://firebasestorage.googleapis.com/v0/b/adnreckitt.appspot.com/o/HIDALGO%20CORTEZ%20ELIANA%20CRISTINA.mp4?alt=media&token=eeefc967-bbdb-4be5-be40-a15116621782')
+owns.push(own16);
+const own17 = new own('0984518253', 'KEVIN', 'https://firebasestorage.googleapis.com/v0/b/adnreckitt.appspot.com/o/LOPEZ%20PEREZ%20KEVIN.mp4?alt=media&token=ae6ba17d-5e53-4a50-87c5-10350a72a630')
+owns.push(own17);
+const own18 = new own('0997051435', 'PEDRO', 'https://firebasestorage.googleapis.com/v0/b/adnreckitt.appspot.com/o/MIRANDA%20CHILLAN%20PEDRO%20LUIS.mp4?alt=media&token=1c313a4c-c06d-46ab-b3f0-b388ea8b9421')
+owns.push(own18);
+const own19 = new own('0984885935', 'ALEJANDRA', 'https://firebasestorage.googleapis.com/v0/b/adnreckitt.appspot.com/o/ALEJADNRA%20BEHR.mp4?alt=media&token=c4cfee8c-5ab6-4119-9df9-1bd654dd5bcc')
+owns.push(own19);
+const own20 = new own('0998250707', 'MARIA', 'https://firebasestorage.googleapis.com/v0/b/adnreckitt.appspot.com/o/MARIA%20BELEN%20JUNCO.mp4?alt=media&token=d64d302e-67f5-4a72-974c-a00d90c38b59')
+owns.push(own20);
+const own21 = new own('0991619999', 'NADYA', 'https://firebasestorage.googleapis.com/v0/b/adnreckitt.appspot.com/o/NADYA%20SALAZAR.mp4?alt=media&token=b3903016-53dd-4198-9ae5-144140533d79')
+owns.push(own21);
+const own22 = new own('0995430848', 'PAOLA', 'https://firebasestorage.googleapis.com/v0/b/adnreckitt.appspot.com/o/PAOLA%20AYALA.mp4?alt=media&token=42c2eb14-bb7c-4b8b-a63e-4ab1487f058b')
+owns.push(own22);
+const own23 = new own('0969244784', 'BARABARA', 'https://firebasestorage.googleapis.com/v0/b/adnreckitt.appspot.com/o/BARBARA%20RUIZ.mp4?alt=media&token=b92065ef-0f20-4189-b43c-b9000e621d0f')
+owns.push(own23);
+const own24 = new own('0995426590', 'MARIA', 'https://firebasestorage.googleapis.com/v0/b/adnreckitt.appspot.com/o/BELEN%20ANDRADE.mp4?alt=media&token=fa6778d3-1b32-49ef-9486-c793a41a353f')
+owns.push(own24);
+const own25 = new own('0984039622', 'CRISTHIAN', 'https://firebasestorage.googleapis.com/v0/b/adnreckitt.appspot.com/o/CHRISTIAN%20ESPINOZA.mp4?alt=media&token=78dad1bc-b31b-4c36-acd0-a7cec1218b7b')
+owns.push(own25);
+const own26 = new own('0984885937', 'JENNY', 'https://firebasestorage.googleapis.com/v0/b/adnreckitt.appspot.com/o/JENNY%20BUENO.mp4?alt=media&token=fc9d91a4-fe2c-4b54-8616-b58df0d0f61d')
+owns.push(own26);
+
+var f = document.querySelector("#ir");
+var consulta = () => {
+    let d = document.querySelector("#_ir");
+    try {
+        let telefono = document.querySelector("#tel").value;
+        verifican = false;
+        verificat = true;
+        verifica = false;
+        verifica_ = false;
+        verifica_ = /^\d{11}$/.test(telefono);
+        verificat = /^\d{10}$/.test(telefono);
+        verifica = (verificat || verifica_);
+        if (!!verifica) {
+            let found = owns.find(e => e.tel == telefono);
+            console.log("dsa");
+            let video = found.video;
+            const htmlm2 = '<td class="iconplay"><button type="button" class="iconplay" data-bs-toggle="modal" data-bs-target="#myModal2"><i class="fa-solid fa-play"></i></button></td>'
+            d.innerHTML = htmlm2;
+            console.log(d)
+            f.innerHTML = "<source  src='" + video + "' type:'video/mp4'>";
+            console.log(video);
+        } else {
+            d.innerHTML = "<P>datos incorrectos</P>"
+        }
+    } catch (e) {
+        throw e;
+    }
+}
